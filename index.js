42ba254c49916c3c01fa6f81f3b74b766937e925
NadyaEvp <t_evp@mail.ru>
2025-05-17 18:16:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('ghbdtn')
+document.addEventListener('DOMContentLoaded', () => {
+    const inputField = document.querySelector('.header__text-field');
+    const iconField = document.querySelector('.header__search');
+
+    console.log(inputField, iconField)
+
+    iconField.addEventListener('click', function() {
+        inputField.classList.toggle('active');
+    });
+})
