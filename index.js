1f44a863d6da19c2efc7d1c02d9d8bd5cf2845e5
NadyaEvp <t_evp@mail.ru>
2025-05-17 18:59:25 +0300

    Добавляем анимацию шапке

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
     const inputField = document.querySelector('.header__text-field');
     const iconField = document.querySelector('.header__search');
-
-    console.log(inputField, iconField)
+    
+    const headerDiv = document.querySelector('.header');
+    const widthSite = document.documentElement.clientWidth;
+    const heightSite = document.documentElement.clientHeight;
 
     iconField.addEventListener('click', function() {
         inputField.classList.toggle('active');
     });
+
+    if (widthSite >= 1024){
+        let lastscroll = 0;
+        function changeHeader(){
+            let pageY = window.pageYOffset;
+
+            if (lastscroll > pageY){
+                headerDiv.style.top = "0px";
+            }
+            else if (pageY >= 200){
+                headerDiv.style.top = `${-heightSite}px`;
+            }
+
+            lastscroll = pageY 
+        }
+
+        window.addEventListener('scroll', changeHeader);
+    }
 })
